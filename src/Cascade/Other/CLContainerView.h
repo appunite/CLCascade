6dc301a77b14fa3ccd0b5e73a722db44cc57b28c
ipodishima <marian.paul2@gmail.com>
2011-11-02 15:36:28 +0100

    Initial import container

---
+++ src/Cascade/Other/CLContainerView.h
@@ -0,0 +1,13 @@
+//
+//  CLContainerView.h
+//  Cascade
+//
+//  Created by Marian Paul on 02/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLContainerView : UIView
+
+@end
---
+++ src/Cascade/Other/CLContainerView.m
@@ -0,0 +1,31 @@
+//
+//  CLContainerView.m
+//  Cascade
+//
+//  Created by Marian Paul on 02/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "CLContainerView.h"
+
+@implementation CLContainerView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
