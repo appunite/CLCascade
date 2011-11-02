b81c440ed9bfdd2580987fea4ee30ca6cb719a2e
ipodishima <marian.paul2@gmail.com>
2011-11-02 17:41:35 +0100

    add the shadow for the normal controller !

--- Examples/Cascade/Cascade/ExampleUITableViewController.m
+++ Examples/Cascade/Cascade/ExampleUITableViewController.m
@@ -32,12 +32,9 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    // add shadow
+    [self addLeftBorderShadowWithWidth:140.0 andOffset:0.0f];
+    
 }
 
 - (void)viewDidUnload
